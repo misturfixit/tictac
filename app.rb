dc9ec7d8093b9fd18b7735c312c465bc6af374a0
misturfixit <japullins420@gmail.com>
2017-10-20 17:23:46 -0400

    grrr... can't figure out how to get players to be able to move for two player...think i need two player to work to be able to try and track stats..not sure how to track the win either

--- app.rb
+++ app.rb
@@ -1,10 +1,6 @@
 require 'sinatra'
 require 'pg'
-require_relative 'console.rb'
-require_relative 'board.rb'
-require_relative 'pc_human.rb'
-require_relative 'pc_seq.rb'
-require_relative 'pc_ran.rb'
+require_relative 'console.rb' 
 require_relative 'dbase.rb'
 enable :sessions
   load './local_env.rb' if File.exists?('./local_env.rb')
@@ -21,48 +17,51 @@ post '/1p_game' do
   # session[:player1] = "x"
   p_name = params[:p_name]
   difc = params[:difc]
-  if difc == "easy"
-    session[:player2] = Playerseq.new("o")
-  elsif  difc == "med"
-    session[:player2] = Playerrand.new("o")
-  else   difc == "hard"
-    session[:player2] = Playerunbeets.new("o")
-  end  
+    if difc == "easy"
+      session[:player2] = Playerseq.new("o")
+    elsif  difc == "med"
+      session[:player2] = Playerrand.new("o")
+    else   difc == "hard"
+      session[:player2] = Playerunbeets.new("o")
+    end  
   redirect '/gameplay'
 end
 ###(((((()))((((((()))))(((2pGAME)))))(((((()))(((((())))))))))###
 post '/humangame' do
   # session[:board] = Board.new
-  session[:player1] = Playerhuman.new("x")
+  # session[:player1] = Playerhuman.new("x")
   session[:player2] = Playerhuman.new("o")
-  session[:p1_name] = params[:p1_name]
-  session[:p2_name] = params[:p2_name]
+  p1_name = params[:p1_name]
+  p2_name = params[:p2_name]
   redirect '/gameplay'
 end
 ###(((((()))((((((()))))((((((GAMEPLAY))))))(((((()))(((((())))))))))###
   get '/gameplay' do
     mssg = params[:mssg] || ""
+
     erb :gameboard, locals:{board: session[:board].board, mssg: mssg}
   end  
 ###(((((()))((((((()))))((((((GAMELOOP))))))(((((()))(((((())))))))))###
   post '/gameloop' do
     move = params[:choice]
+
     
     if  session[:board].val_spot(session[:board].board, move) == true
         session[:board].place_marker("x", move)
+      ##vvvv not sure about this below.. trying to get 2 player to work
+    # elsif   session[:board].val_spot(session[:board].board, move) == true  
+    #         session[:board].place_marker("o", move)
     else 
       redirect '/gameplay?mssg=invalid spot choose again'
     end   
     if  session[:board].winr(session[:board].board) || session[:board].full?
       redirect '/gameover'
-    elsif  
+    # elsif 
+    #   hum = session[:player2].move(session[:board].board)  
+    #         session[:board].place_marker("o", hum)
+    else  
       ai =  session[:player2].move(session[:board].board)  
             session[:board].place_marker("o", ai)
-            ##vvvv not sure about this below.. trying to get 2 player to work
-    elsif   session[:board].val_spot(session[:board].board, move) == true  
-            session[:board].place_marker("o", move)
-    else 
-      redirect '/gameplay?mssg=invalid spot choose again'
     end
     if  session[:board].winr(session[:board].board) || session[:board].full?  
       redirect '/gameover'
@@ -82,7 +81,7 @@ get '/gameover' do
   end
 end
 ###(((((()))((((((()))))((((((SCOREBOARD))))))(((((()))(((((())))))))))###
-get '/score' do
+get '/scores' do
   # list = 
   result = get_score_b(score)
   list = params[:list]
