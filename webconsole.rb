bd6f8cd2b750ea2dbaa07535f64c9f8d4d54b6be
misturfixit <japullins420@gmail.com>
2017-10-24 19:49:03 -0400

    wwowwwsers...so i made a change that broke the whole app... then pushed before i checked it...thank gawd i knew which change....gonna re-deploy

--- webconsole.rb
+++ webconsole.rb
@@ -8,14 +8,14 @@ class Console
   attr_accessor :board, :player1, :player2, :marker, :current_player, :showboard
 ###(((((()))((((((()))))((((((INIT))))))(((((()))))))(((((())))))))))###
   def  initialize() 
-    # @board = Board.new 
-    session[:player1] = @player1 
-    session[:player2] = @player2
-    session[:current_player] = session[:player1]
+    @board = Board.new 
+    @player1 = player1
+    @player2 = player2
+    @current_player = player1
   
   end  
   def player_sel()
-    p      " MAybe  #{current_player}here's the current___player_________????11!!!!!"
+    p "  maybe #{current_player}here's the current___player_________????11!!!!!"
     if current_player == @player1
         @current_player = player2
     else current_player == @player2
