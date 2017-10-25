a21c266a79ba283d7e0b1171e9cea0913a77f8c7
misturfixit <japullins420@gmail.com>
2017-10-24 21:02:32 -0400

    welll....lestrythis...hashing out all dbase stuff

--- dbase.rb
+++ dbase.rb
@@ -1,65 +1,65 @@
-require 'pg'
-	load './local_env.rb' if File.exist?('./local_env.rb')
+# require 'pg'
+# 	load './local_env.rb' if File.exist?('./local_env.rb')
 
-###((((((()))))))(((((((()))))))(MAKETABLE)((((((()))))))((((((()))))))###
-def maketable()
-	begin
-	  db_info = {
-	    host: ENV['RDS_HOST'],
-	    port: ENV['RDS_PORT'],
-	    dbname: ENV['RDS_DB_NAME'],
-	    user: ENV['RDS_USERNAME'],
-	    password: ENV['RDS_PASSWORD']
-  	  }
-    d_base = PG::Connection.new(db_info)
-    d_base.exec ("CREATE TABLE public.scoreboard (
-              p1_name text,
-              p2_name text,
-              winner text,
-              time time)");
-	rescue PG::Error => e
-	   puts e.message
-	ensure
-	   d_base.close if d_base
-	end
-end	
-###((((((()))))))((((((()))))))(UpD_TABLE)((((((()))))))((((((()))))))###
-def add_entry(data)
-  begin
-    db_info = {
-      host: ENV['RDS_HOST'],
-      port: ENV['RDS_PORT'],
-      dbname: ENV['RDS_DB_NAME'],
-      user: ENV['RDS_USERNAME'],
-      password: ENV['RDS_PASSWORD']
-      }
-    d_base = PG::Connection.new(db_info)
-    d_base.exec("INSERT INTO public.scoreboard(p1_name,p2_name,winner,time)VALUES('#{p1_name}','#{p2_name},'#{winner}','#{time}')");            
-  rescue PG::Error => e
-    puts e.message
-  ensure
-    d_base.close if d_base
-  end  
- end
-###((((((()))))))((((((()))))))(SCOREBOARD)((((((()))))))((((((()))))))###
-def get_scores(score)
-  begin
-    db_info = {
-    host: ENV['RDS_HOST'],
-    port: ENV['RDS_PORT'],
-    dbname: ENV['RDS_DB_NAME'],
-    user: ENV['RDS_USERNAME'],
-    password: ENV['RDS_PASSWORD']
-    }
-  d_base = PG::Connection.new(db_info)
+# ###((((((()))))))(((((((()))))))(MAKETABLE)((((((()))))))((((((()))))))###
+# def maketable()
+# 	begin
+# 	  db_info = {
+# 	    host: ENV['RDS_HOST'],
+# 	    port: ENV['RDS_PORT'],
+# 	    dbname: ENV['RDS_DB_NAME'],
+# 	    user: ENV['RDS_USERNAME'],
+# 	    password: ENV['RDS_PASSWORD']
+#   	  }
+#     d_base = PG::Connection.new(db_info)
+#     d_base.exec ("CREATE TABLE public.scoreboard (
+#               p1_name text,
+#               p2_name text,
+#               winner text,
+#               time time)");
+# 	rescue PG::Error => e
+# 	   puts e.message
+# 	ensure
+# 	   d_base.close if d_base
+# 	end
+# end	
+# ###((((((()))))))((((((()))))))(UpD_TABLE)((((((()))))))((((((()))))))###
+# def add_entry(data)
+#   begin
+#     db_info = {
+#       host: ENV['RDS_HOST'],
+#       port: ENV['RDS_PORT'],
+#       dbname: ENV['RDS_DB_NAME'],
+#       user: ENV['RDS_USERNAME'],
+#       password: ENV['RDS_PASSWORD']
+#       }
+#     d_base = PG::Connection.new(db_info)
+#     d_base.exec("INSERT INTO public.scoreboard(p1_name,p2_name,winner,time)VALUES('#{p1_name}','#{p2_name},'#{winner}','#{time}')");            
+#   rescue PG::Error => e
+#     puts e.message
+#   ensure
+#     d_base.close if d_base
+#   end  
+#  end
+# ###((((((()))))))((((((()))))))(SCOREBOARD)((((((()))))))((((((()))))))###
+# def get_scores(score)
+#   begin
+#     db_info = {
+#     host: ENV['RDS_HOST'],
+#     port: ENV['RDS_PORT'],
+#     dbname: ENV['RDS_DB_NAME'],
+#     user: ENV['RDS_USERNAME'],
+#     password: ENV['RDS_PASSWORD']
+#     }
+#   d_base = PG::Connection.new(db_info)
 
-  list = d_base.exec("SELECT * FROM public.scoreboard")
+#   list = d_base.exec("SELECT * FROM public.scoreboard")
             
-  rescue PG::Error => e
-    puts e.message
-  ensure
-    d_base.close if d_base
-  end
+#   rescue PG::Error => e
+#     puts e.message
+#   ensure
+#     d_base.close if d_base
+#   end
 
-end 
+# end 
 ###((((((()))))))((((((()))))))((((((()))))))((((((()))))))###
