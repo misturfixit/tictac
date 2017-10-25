8cfa9ff046964cb12c0eb3b515fd2e5b24a7c73f
misturfixit <japullins420@gmail.com>
2017-10-25 16:58:59 -0400

    wheehooo...everything but the dbase is on...sumfin weird with my unbeetl.vs unbeetle...does fine human vs un...lookin into it

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ enable :sessions
   get '/' do
     session[:board] = Board.new
     # session[:player1] = Playerhuman.new("x",params[:p1_name]) 
-    session[:current_player] = session[:player1]
+    # session[:current_player] = session[:player1]
           # p "__________________#{session[:current_player].marker}" 
     session[:console] = Console.new
       # maketable()
@@ -18,49 +18,89 @@ enable :sessions
 ###(((((()))((((((()))))((((1pGAME))))((((())))(((((())))))))))###
   post '/1p_game' do
     difc = params[:difc]
-      if  difc == "human" 
+    difc2 = params[:difc2]
+    p "______difcSELECTION____________#{difc}__________HERE___________"
+    p "________difc2SELECTION__________#{difc2}____________THERE_________" 
+
+      if  difc == "human"
         session[:player1] = Playerhuman.new("x",params[:p1_name]) 
       elsif  difc == "easy"
-        session[:player2] = Playerseq.new("o",params[:p2_name])
+        session[:player1] = Playerseq.new("x",params[:p1_name])
       elsif  difc == "med"
+        session[:player1] = Playerrand.new("x",params[:p1_name])
+      else   
+        session[:player1] = Playerunbeets.new("x",params[:p1_name])
+      end
+      if  difc2 == "human"
+        session[:player2] = Playerhuman.new("o",params[:p2_name]) 
+      elsif  difc2 == "easy"
+        session[:player2] = Playerseq.new("o",params[:p2_name])
+      elsif  difc2 == "med"
         session[:player2] = Playerrand.new("o",params[:p2_name])
-      else   difc == "hard"
+      else  
         session[:player2] = Playerunbeets.new("o",params[:p2_name])
-      end  
+      end
+      session[:current_player] = session[:player1]    
+      
+      p "_______PLAYER1#{session[:player1].class}is here________________"
+      p "_______PLAYER2#{session[:player2].class}is here________________"
     redirect '/gameplay'
   end
 ###(((((()))((((((()))))((((2pGAME)))))(((((()))(((((())))))))))###
-  post '/humangame' do
-    session[:player1] = Playerhuman.new("x",params[:p1_name])
-    session[:player2] = Playerhuman.new("o",params[:p2_name])
-    #       p  "_______PLAYER1 is #{session[:player1].marker}here.....???_______________!!!!"
-    #       p  "_______PLAYER2 is #{session[:player2].marker}HERE.....???_______________!!!!"
-    #       p  "________in post__CURRENTPLAYER is#{session[:current_player].marker}here....___________!!!!"
-    redirect '/gameplay'
-  end
+  # post '/humangame' do
+  #   session[:player1] = Playerhuman.new("x",params[:p1_name])
+  #   session[:player2] = Playerhuman.new("o",params[:p2_name])
+  #   #       p  "_______PLAYER1 is #{session[:player1].marker}here.....???_______________!!!!"
+  #   #       p  "_______PLAYER2 is #{session[:player2].marker}HERE.....???_______________!!!!"
+  #   #       p  "________in post__CURRENTPLAYER is#{session[:current_player].marker}here....___________!!!!"
+  #   redirect '/gameplay'
+  # end
 ###(((((()))((((((()))))(((GAMEPLAY))))(((((()))(((((())))))))))###
+ ###(((((()))((((((()))))((((((gameboard))))))(((((()))(((((())))))))))###
+  get '/gameboard' do
+  mssg = params[:mssg] || ""
+     erb :gameboard, locals:{board: session[:board].board, mssg: mssg}
+  end
+    ###(((((()))((((((()))))(((((())))))(((((()))(((((())))))))))###
   get '/gameplay' do
     mssg = params[:mssg] || ""
-
-    erb :gameboard, locals:{board: session[:board].board, mssg: mssg}
+      p "________CURRENTPLAYERCLASS________________#{session[:current_player].class}______________"  
+    if session[:current_player].class == Playerhuman
+      redirect '/gameboard?mssg='+mssg
+        # erb :gameboard, locals:{board: session[:board].board, mssg: mssg}
+      else
+        p "__________#{session[:current_player]}is here__________________________________"
+        ai =  session[:current_player].move(session[:board].board)  ###   AI move
+              session[:board].place_marker(session[:current_player].marker, ai) 
+        if session[:current_player] == session[:player1]      ### checking the current_player
+           session[:current_player] = session[:player2] 
+        else
+          session[:current_player] = session[:player1]
+        end  
+      end 
+      if  session[:board].winr(session[:board].board) || session[:board].full?  ###final check for win or tie
+      
+        redirect '/gameover'
+      else  
+        redirect '/gameplay'
+      end        
   end  
 ###(((((()))((((((()))))((((GAMELOOP))))(((((()))(((((())))))))))###
 post '/gameloop' do
   move = params[:choice]
-  data = session[:p1_name],session[:p2_name]
-
+  
     if  session[:board].val_spot(session[:board].board, move) == true    ### checking for valid spot
         session[:board].place_marker(session[:current_player].marker, move)   #### placing the actual marker
     else 
         redirect '/gameplay?mssg=invalid spot choose again'
     end        
     if  session[:board].winr(session[:board].board) || session[:board].full?    ### checking for win or tie
-      redirect '/gameover?data='+data    
+      redirect '/gameover'    
     else 
               # p  "________before playerchange___CURRENTPLAYER is#{session[:current_player].marker}here....___________!!!!"
               # p  "_______PLAYER2 is #{session[:player2].marker}HERE.....???_______________!!!!"
               # p  "_______PLAYER1 is #{session[:player1].marker}here.....???_______________!!!!"
-      if session[:player2].class == Playerhuman        ### checking to see if player2 is human   
+      if session[:current_player].class == Playerhuman        ### checking to see if player2 is human   
         if session[:current_player] == session[:player1]      ### checking the current_player
            session[:current_player] = session[:player2]       ### changing current_player
               # p  "_______firstplayerchange____CURRENTPLAYER is#{session[:current_player].marker}here....___________!!!!"
@@ -70,14 +110,13 @@ post '/gameloop' do
               # p  "_____secondplayerchange__CURRENTPLAYER is#{session[:current_player].marker}here....___________!!!!"
           redirect '/gameplay' 
         end  
-      else  
-        
-        ai =  session[:player2].move(session[:board].board)  ###   AI move
-            session[:board].place_marker("o", ai)
+      else
+        ai =  session[:current_player].move(session[:board].board)  ###   AI move
+        session[:board].place_marker(session[:current_player].marker, ai) 
       end
     end  
     if  session[:board].winr(session[:board].board) || session[:board].full?  ###final check for win or tie
-      redirect '/gameover?data='+data
+      redirect '/gameover'
     else  
       redirect '/gameplay'
     end  
@@ -98,8 +137,7 @@ end
 #   list = params[:list]
 #   erb :scoreboard, locals:{list:list,result:result}
 # end       
-    ###(((((()))((((((()))))(((((())))))(((((()))(((((())))))))))###
-    ###(((((()))((((((()))))(((((())))))(((((()))(((((())))))))))###
+   
     ###(((((()))((((((()))))(((((())))))(((((()))(((((())))))))))###
     
 ###(((((()))((((((()))))(((((())))))(((((()))(((((())))))))))###
