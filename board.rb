e7f03577566c3fda6873b704287ca5a7a5160111
misturfixit <japullins420@gmail.com>
2017-12-20 22:54:54 -0500

    making board expansion function

--- board.rb
+++ board.rb
@@ -2,34 +2,37 @@ class Board
 
 	attr_accessor :board, :marker, :choice
 	def	initialize()
-		@board = ["","","","","","","","",""]
+		boarsize_h = 3
+		@board = Array.new((boarsize_h * boarsize_h), "")
+		puts @Board
 	end
+	initialize()
 ###(((((()))((()))((()))(((Marker)))((())))((())))((())((())))###
   def place_marker(marker,choice)
     # p "#{choice}is this getting there??????????????"
 	  @board[choice.to_i - 1] = marker
-		@board					
-	end	
+		@board
+	end
 ###(((((()))((()))((()))(((ValidSpot)))((())))((())))((())((())))###
 	def val_spot(board,marker)
     bob = marker.to_i
-    choice = bob -1 
+    choice = bob -1
     if choice > 8  || choice < 0
       false
     elsif @board[choice] == "x" || @board[choice] == "o"
       false
-    else 
-      true  
-    end     
-  end 
+    else
+      true
+    end
+  end
 ###(((((()))((()))((()))(((FullBoard)))((())))((())))((())((())))###
 	def full?()
-    if  @board.include?("") 
+    if  @board.include?("")
       false
     else
       true
     end
-  end    
+  end
 ###(((((()))((()))((()))(((WinCombos)))((())))((())))((())((())))###
 	def win_combos(board)
    [[@board[0],@board[1],@board[2]],
@@ -50,12 +53,12 @@ class Board
       elsif comb[0] == "o" && comb[1] == "o" && comb[2] == "o"
         win << true
       else
-        win << false    
+        win << false
       end
     end
      win.include?(true)
   end
 
-end	
+end
 ###(((((()))((()))((()))((()))((())))((())))((())((())))###
 ###(((((()))((()))((()))((()))((())))((())))((())((())))###
---
+++ expand.rb
@@ -0,0 +1,7 @@
+def	board_size()
+  boardsize_h = 30
+  board = Array.new((boardsize_h * boardsize_h), "")
+  p board
+  puts "hello world"
+end
+board_size()
